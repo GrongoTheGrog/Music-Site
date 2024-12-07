8d45bf2411df405c20a4832d704469edde0f4826
Bruno Hugo <brunohugo2007@gmail.com>
2024-12-07 00:54:10 -0300

    created a fetch to Jamendo API to get free songs to play on my app

--- api/api.js
+++ api/api.js
@@ -1,13 +1,44 @@
-const API_KEY = '1797a491';
+const url = 'https://api.jamendo.com/v3.0/albums/?client_id=1797a491&namesearch=jazz&limit=14';
 
-async function fetchData() {
+export async function fetchData() {
   try{
-    const response = (await fetch(`https://api.jamendo.com/v3.0/albums?artist_name=we+are+fm&client_id=${API_KEY}&format=json`));
+    const response = (await fetch(url));
 
-    const data = await response.json();
+    if (!response.ok){
+      throw new Error(`HTTP error, status: ${response.status}`);
+    }
+
+    data = await response.json();
 
     console.log(data);
+    
+    return data;
   } catch(error){
     console.error(error);
   }
 }
+
+async function display(){
+  const data = await fetchData();
+  const albums = data.results;
+
+  let inner = ``;
+
+  for(let i = 0; i < 14; i++){
+
+    inner += `
+    <div>
+      <img src="${albums[i].image}" class="image">
+      
+      <p>
+        Name: ${albums[i].name}
+      </p>
+    </div>
+    ` 
+  }
+
+  const element = document.getElementById('div1');
+  element.innerHTML = inner;
+}
+
+display();
--- scripts/content.js
+++ scripts/content.js
@@ -1,34 +1,80 @@
-chainHTML = ``;
-
-albumList.forEach((album) => {
-  const chainId = document.querySelector('.music-chain');
-
-  chainHTML += `
-    <div class="music-preview">
-      <a href="playlist.html">
-        <div class="image""> 
-          <div class="play-icon-button">
-          <i class="fa fa-play play"></i>
-          </div>
-          <img src="${album.image}" class="image-display"> 
-        </div>
-      </a>
-
-        <div class="music-stats">
-          <p class="name">
-            ${album.name}
-          </p>
-
-          <p class="type">
-            Album
-          </p>
-
-          <p class="band">
-            ${album.band}
-          </p>
-        </div>
-      </div>
+async function fetchData(param) {
+  try{
+    let response;
+    if (!param){
+      response = (await fetch('https://api.jamendo.com/v3.0/albums/?client_id=1797a491&namesearch=jazz&limit=14'));
+    }else{
+      response = (await fetch(`https://api.jamendo.com/v3.0/albums/?client_id=1797a491&namesearch=${param}&limit=14`));
       
-  `
-  chainId.innerHTML = chainHTML;
+    }
+    if (!response.ok){
+      throw new Error(`HTTP error, status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+    
+    return data;
+  } catch(error){
+    console.error(error);
+  }
+}
+
+
+const chainId = document.querySelector('.music-chain');
+
+async function display(a) {
+
+  let chainHTML = ``;
+  try{
+    const data = await fetchData(a);
+    const albums = data.results;
+    console.log('hi')
+
+    for (let i = 0; i < Math.min(14, albums.length); i++){
+      chainHTML += `
+      <div class="music-preview">
+        <a href="playlist.html">
+          <div class="image""> 
+            <div class="play-icon-button">
+            <i class="fa fa-play play"></i>
+            </div>
+            <img src="${albums[i].image}" class="image-display"> 
+          </div>
+        </a>
+
+          <div class="music-stats">
+            <p class="name">
+              ${albums[i].name}
+            </p>
+
+            <p class="type">
+              Album
+            </p>
+
+            <p class="band">
+              ${albums[i].artist_name}
+            </p>
+          </div>
+        </div>
+        `
+    }
+    chainId.innerHTML = chainHTML;
+  } catch (error){
+    console.error('Error in display: ', error)
+  }
+} 
+
+display();
+
+const input = document.querySelector('.input-center');
+const button = document.querySelector('.button-search-header');
+
+button.addEventListener('click', () => {
+  const value = input.value;
+
+  display(value);
+
 })
+
